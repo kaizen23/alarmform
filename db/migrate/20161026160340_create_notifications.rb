886e4db1ce2bd9c4e7f636b8ae682e46de03994f
kaizen <neuralbox@op.pl>
2016-10-26 09:20:21 -0700

    scaffolding notifications containg email descriptions

---
+++ db/migrate/20161026160340_create_notifications.rb
@@ -0,0 +1,11 @@
+class CreateNotifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notifications do |t|
+      t.text :description
+      t.string :category
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
