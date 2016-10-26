6a78b814e8fbf7ad7dbf9e887ba1f785441e17e2
kaizen <neuralbox@op.pl>
2016-10-26 10:13:36 -0700

    add validations email to notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,9 +1,17 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class Notification < ApplicationRecord
 #add validates
 CATEGORY_TYPES = ["Awaria", "Uszkodzenie", "Usterka" ]
 #dont be blank description email
-	validates  :email, presence: true
-	validates_length_of :description, :minimum  => 100, :message => "Opis musi miec przynajmniej 100 znakow"
+	validates  :email, presence: true, email: true
+	validates :description, presence: true, length: { minimum: 100 }
 #Category must be included from the list
 	validates :category, inclusion: CATEGORY_TYPES
 end
