69f2610f66eb513ecf6f33d620030495af3eb5c8
kaizen <neuralbox@op.pl>
2016-10-27 11:55:53 -0700

    add status to table notifications use migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026190927) do
+ActiveRecord::Schema.define(version: 20161027185214) do
 
   create_table "notifications", force: :cascade do |t|
     t.text     "description"
     t.string   "category"
     t.string   "email"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "status",      default: true
   end
 
   create_table "orders", force: :cascade do |t|
