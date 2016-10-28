315b9bc61bb431d1a1153c3ee1b55f7b942c536a
kaizen <neuralbox@op.pl>
2016-10-28 11:54:17 -0700

    add hash which allows filtering notifications by category

--- app/services/secsys.rb
+++ app/services/secsys.rb
@@ -1,9 +1,24 @@
+	#all testing in rails console
 class Secsys 
   #x is number of quantity record you want download
   def display(x= '1', hash = {})
+  
+  #example of arguments
+  #display(2,{:category=>'Awaria', :cost=>'100', :email=>'it@szpital4.bytom.pl'})
   @x = x 
+  
+  if (hash[:category] =='Uszkodzenie' || hash[:category] =='Awaria' || hash[:category] =='Usterka') 
+  @category = 'AND category = "' + hash[:category] + '"'
+  end
+  
+  #Don't enough time to end rest hash
+  #SELECT notifications.id FROM notifications LEFT OUTER JOIN orders ON notifications.id=orders.notification_id where cost = 50;
+  #if (hash[:cost] =='100' || hash[:cost] =='50' || hash[:cost] =='10' || hash[:cost] =='1') 
+  #@id_cost = 'AND notifications.id = "' + hash[:cost] + '"'
+  #end
+  
   #fetch date
-  sql = "SELECT * FROM notifications WHERE status = 1 ORDER BY created_at ASC LIMIT #{@x} "
+  sql = "SELECT * FROM notifications WHERE status = 1  #{@category} ORDER BY created_at ASC LIMIT #{@x} "
   #change status to read which equal 0
   update_sql = "UPDATE notifications SET status = 0 WHERE status = 1 ORDER BY created_at ASC LIMIT #{@x} "
   #execute sql using ActiveRecord
