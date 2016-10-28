686b016bac7bf47efd2b69b7801dea1b6a3aa99c
kaizen <neuralbox@op.pl>
2016-10-28 09:55:25 -0700

    add comment to class secsys

--- app/services/secsys.rb
+++ app/services/secsys.rb
@@ -1,11 +1,15 @@
 class Secsys 
- 
+  #x is number of quantity record you want download
   def display(x: '1')
   @x = x 
+  #fetch date
   sql = "SELECT * FROM notifications WHERE status = 1 ORDER BY created_at ASC LIMIT #{@x} "
+  #change status to read which equal 0
   update_sql = "UPDATE notifications SET status = 0 WHERE status = 1 ORDER BY created_at ASC LIMIT #{@x} "
+  #execute sql using ActiveRecord
 	result = ActiveRecord::Base.connection.execute(sql)
 	execute_update = ActiveRecord::Base.connection.execute(update_sql)
+	#return the score
 	return result
   end
   
